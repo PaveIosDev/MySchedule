0ffafb0c12348cbecc94bb9de6497cbad4644474
Pavelweb22 <pavelweb2016@yandex.ru>
2023-04-11 21:28:48 +0300

    create button

--- MySchedule/Controllers/ScheduleViewController.swift
+++ MySchedule/Controllers/ScheduleViewController.swift
@@ -10,10 +10,21 @@ import FSCalendar
 
 class ScheduleViewController: UIViewController {
 
+    var calendarHeightConstraint: NSLayoutConstraint!
+    
     private var calendar: FSCalendar = {
         let calendar = FSCalendar()
         calendar.translatesAutoresizingMaskIntoConstraints = false
         return calendar
+    }()
+    
+    let showHideButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Open calendar", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.titleLabel?.font = UIFont(name: "Avenir Next Demi Bold", size: 14)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
     }()
     
     override func viewDidLoad() {
@@ -21,6 +32,39 @@ class ScheduleViewController: UIViewController {
         
         view.backgroundColor = .white
         title = "Schedule"
+        
+        calendar.dataSource = self
+        calendar.delegate = self
+        
+        setConstraints()
+    }
+}
+
+// MARK: - FSCalendarDataSource, FSCalendarDelegate
+
+extension ScheduleViewController: FSCalendarDataSource, FSCalendarDelegate {
+    func calendar(_ calendar: FSCalendar, boundingRectWillChange bounds: CGRect, animated: Bool) {
+        calendarHeightConstraint.constant = bounds.height
+        view.layoutIfNeeded()
+    }
+}
+
+// MARK: - setConstraints
+
+extension ScheduleViewController {
+ 
+    func setConstraints() {
+        
+        view.addSubview(calendar)
+        
+        calendarHeightConstraint = NSLayoutConstraint(item: calendar, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 300)
+        calendar.addConstraint(calendarHeightConstraint)
+        
+        NSLayoutConstraint.activate([
+            calendar.topAnchor.constraint(equalTo: view.topAnchor, constant: 90),
+            calendar.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            calendar.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+        ])
     }
 
 }
